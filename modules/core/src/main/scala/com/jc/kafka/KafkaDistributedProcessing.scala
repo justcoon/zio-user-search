d55acd02aff2020e3f9724a94c95dcf8f17593c2
Peter Kotula <peto.kotula@yahoo.com>
2022-01-09 01:27:08 +0100

    distributed processing

--- modules/core/src/main/scala/com/jc/kafka/KafkaDistributedProcessing.scala
+++ modules/core/src/main/scala/com/jc/kafka/KafkaDistributedProcessing.scala
@@ -6,6 +6,7 @@ import zio.clock.Clock
 import zio.{Chunk, Exit, Fiber, RManaged, URIO, ZIO}
 import zio.kafka.consumer.{CommittableRecord, Consumer, ConsumerSettings, Subscription}
 import zio.kafka.serde.Serde
+import zio.logging.{Logger, Logging}
 import zio.stream.ZStream
 
 import scala.jdk.CollectionConverters._
@@ -14,7 +15,7 @@ import java.util.concurrent.ConcurrentHashMap
 object KafkaDistributedProcessing {
 
   // https://github.com/notxcain/aecor/tree/master/modules/kafka-distributed-processing/src/main/scala/aecor/kafkadistributedprocessing
-  def start[R <: Clock with Blocking](
+  def start[R <: Clock with Blocking with Logging](
     name: String,
     settings: ConsumerSettings,
     process: Int => ZIO[R, Throwable, Unit]) = {
@@ -26,43 +27,51 @@ object KafkaDistributedProcessing {
 
       val store = new ConcurrentHashMap[Int, URIO[R, Fiber.Runtime[Throwable, Unit]]]()
 
-      val run: ZIO[R, Throwable, Unit] =
-        c.partitionedAssignmentStream[R, Array[Byte], Array[Byte]](Serde.byteArray, Serde.byteArray)
-          .mapM {
-            newPartitions: Chunk[(
-              TopicPartition,
-              ZStream[R, Throwable, CommittableRecord[Array[Byte], Array[Byte]]])] =>
-              val ops = store.keys().asScala.toSet
+      val run: ZIO[R, Throwable, Unit] = {
+        ZIO.accessM[R] { env =>
+          val logger = env.get[Logger[String]]
+          c.partitionedAssignmentStream[R, Array[Byte], Array[Byte]](Serde.byteArray, Serde.byteArray)
+            .mapM {
+              newPartitions: Chunk[(
+                TopicPartition,
+                ZStream[R, Throwable, CommittableRecord[Array[Byte], Array[Byte]]])] =>
+                val ops = store.keys().asScala.toSet
 
-              val nps = newPartitions.map { case (topicPartition, _) =>
-                topicPartition.partition()
-              }.toSet
+                val nps = newPartitions.map { case (topicPartition, _) =>
+                  topicPartition.partition()
+                }.toSet
 
-              val removed: Set[ZIO[R, Throwable, Unit]] = ops.filterNot(nps.contains).map { partition =>
-                Option(store.remove(partition)).fold[ZIO[R, Throwable, Unit]](ZIO.unit) {
-                  p: URIO[R, Fiber.Runtime[Throwable, Unit]] =>
-                    for {
-                      fiber <- p
-                      _ <- fiber.interrupt
-                    } yield ()
+
+                val removed: Set[ZIO[R, Throwable, Unit]] = ops.filterNot(nps.contains).map { partition =>
+                  Option(store.remove(partition)).fold[ZIO[R, Throwable, Unit]](ZIO.unit) {
+                    p: URIO[R, Fiber.Runtime[Throwable, Unit]] =>
+                      for {
+                        fiber <- p
+                        _ <- fiber.interrupt
+                        _ <- logger.debug(s"process - partition: ${partition} - stopped")
+                      } yield ()
+                  }
                 }
-              }
 
-              val updated: Set[URIO[R, Fiber.Runtime[Throwable, Unit]]] = nps.map { partition =>
-                store.computeIfAbsent(
-                  partition,
-                  { partition =>
-                    process(partition).forkDaemon
-                  })
-              }
+                val updated: Set[URIO[R, Fiber.Runtime[Throwable, Unit]]] = nps.map { partition =>
+                  store.computeIfAbsent(
+                    partition,
+                    { _ =>
+                      logger.debug(s"process - partition: ${partition} - starting") *>
+                        process(partition).forkDaemon
+                    })
+                }
 
-              for {
-                _ <- ZIO.collectAll(removed)
-                _ <- ZIO.collectAll(updated)
-              } yield ()
+                for {
+                  _ <- logger.debug(s"process - partitions: ${nps.mkString(",")}")
+                  _ <- ZIO.collectAll(removed)
+                  _ <- ZIO.collectAll(updated)
+                } yield ()
 
-          }
-          .runDrain
+            }
+            .runDrain
+        }
+      }
 
       subscribe.flatMap(_ => run)
     }
